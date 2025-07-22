c56c72e35d9b9a68f25953c6c5c79db167dad35a
Mohammed Suhaib <suhaibfullstackdeveloper@gmail.com>
2025-07-22 22:53:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base : "/suhaib_portfolio",
+  base : "/portfolio",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
