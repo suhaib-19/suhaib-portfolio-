dc96888bc95b5eda60e8209d5d0466855c8bdacb
unknown <suhaibfullstackdeveloper@gmail.com>
2025-11-19 19:41:52 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   base : process.env.VITE_BASE_PATH || "/suhaib-portfolio-"
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+ 
 })
